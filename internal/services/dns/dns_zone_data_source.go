c90f6f4e659dc48ed84191d4b44a1e20f8e876a5
Alex Wilcox <alex.wilcox@cloudsecure.ltd>
2024-12-08 20:48:58 +0000

    Update DNS API version (local override)

--- internal/services/dns/dns_zone_data_source.go
+++ internal/services/dns/dns_zone_data_source.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/dns/2018-05-01/zones"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dns/2023-07-01-preview/zones"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
