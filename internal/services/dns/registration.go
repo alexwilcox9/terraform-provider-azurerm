9634ba9b6b915b9c80d262de794f48cb4f911296
Alex Wilcox <alex.wilcox@cloudsecure.ltd>
2024-12-12 22:29:54 +0000

    Add TLSA records

--- internal/services/dns/registration.go
+++ internal/services/dns/registration.go
@@ -68,6 +68,7 @@ func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 func (r Registration) DataSources() []sdk.DataSource {
 	return []sdk.DataSource{
 		DnsDSRecordDataResource{},
+		DnsTLSARecordDataResource{},
 	}
 }
 
@@ -75,5 +76,6 @@ func (r Registration) DataSources() []sdk.DataSource {
 func (r Registration) Resources() []sdk.Resource {
 	return []sdk.Resource{
 		DnsDSRecordResource{},
+		DnsTLSARecordResource{},
 	}
 }
